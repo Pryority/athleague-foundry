463f9ebdce945df29f45c5440bada8ad70f605d7
pryority <matthewapryor@gmail.com>
2023-04-23 17:26:34 -0400

    update tests -- all passing

--- test/Course.t.sol
+++ test/Course.t.sol
@@ -15,9 +15,10 @@ contract CourseTest is Test {
 
     function test_AddCheckpoint() public {
         course.addCheckpoint(123, 456);
-        course.finalizeCourse();
+        course.addCheckpoint(654, 321);
+        course.build();
         Course.Checkpoint[] memory checkpoints = course.getCheckpoints();
-        assertEq(checkpoints.length, 1);
+        assertEq(checkpoints.length, 2);
         assertEq(checkpoints[0].coord.lat, 123);
         assertEq(checkpoints[0].coord.long, 456);
         assertEq(checkpoints[0].sequence, 0);
@@ -26,21 +27,22 @@ contract CourseTest is Test {
     function test_RemoveCheckpoint() public {
         course.addCheckpoint(123, 456);
         course.removeCheckpoint(0);
-        vm.expectRevert("At least one checkpoint must be added.");
-        course.finalizeCourse();
+        vm.expectRevert("At least two checkpoints must be added.");
+        course.build();
     }
 
-    function test_FinalizeCourse() public {
+    function test_Build() public {
         course.addCheckpoint(123, 456);
-        course.finalizeCourse();
-        bool finalized = course.finalized();
-        assertEq(finalized, true);
+        course.addCheckpoint(101, 202);
+        course.build();
+        bool built = course.built();
+        assertEq(built, true);
     }
 
     function test_GetCheckpoints() public {
         course.addCheckpoint(123, 456);
         course.addCheckpoint(789, 101112);
-        course.finalizeCourse();
+        course.build();
         Course.Checkpoint[] memory checkpoints = course.getCheckpoints();
         assertEq(checkpoints.length, 2);
         assertEq(checkpoints[0].coord.lat, 123);
@@ -54,6 +56,7 @@ contract CourseTest is Test {
     function test_OnlyOwnerModifier() public {
         vm.prank(owner);
         Course otherCourse = new Course();
+        course.addCheckpoint(654, 321);
         try otherCourse.addCheckpoint(123, 456) {
             assertTrue(false, "Function should have reverted.");
         } catch Error(string memory reason) {
@@ -68,7 +71,7 @@ contract CourseTest is Test {
         } catch {
             assertTrue(false, "Unexpected error.");
         }
-        try otherCourse.finalizeCourse() {
+        try otherCourse.build() {
             assertTrue(false, "Function should have reverted.");
         } catch Error(string memory reason) {
             assertEq(reason, "Only the owner can perform this operation.");
@@ -77,16 +80,20 @@ contract CourseTest is Test {
         }
     }
 
-    function test_NotFinalizedModifier() public {
+    function test_NotBuiltModifier() public {
         course.addCheckpoint(123, 456);
-        course.finalizeCourse();
-        vm.expectRevert("Course has already been finalized.");
+        course.addCheckpoint(654, 321);
+        course.build();
+        vm.expectRevert("Course has already been built.");
         course.addCheckpoint(789, 101112);
     }
 
     function test_MarkCheckpointCompleted() public {
         course.addCheckpoint(123, 456);
-        course.finalizeCourse();
+        course.addCheckpoint(654, 321);
+        course.build();
+        vm.prank(owner);
+        course.startCourse();
         course.markCheckpointCompleted(0, owner);
     }
 }
