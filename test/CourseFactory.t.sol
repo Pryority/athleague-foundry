b0e7ad4d90e2fa88005704004c26d54d7fb1f916
pryority <matthewapryor@gmail.com>
2024-03-14 15:22:43 -0400

    🧪 Test createCheckpoint

---
+++ test/CourseFactory.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+/*
+import "forge-std/Test.sol";
+import "../src/CourseFactory.sol";
+
+contract CourseFactoryTest is Test {
+    function setUp() public {
+        factory = new CourseFactory();
+    }
+
+    function test_createCourse() public {
+        factory.createCourse()
+    }
+}*/
