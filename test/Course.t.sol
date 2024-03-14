be158c452a5f5f23924b27a44fc1b960cb7fe8cc
pryority <matthewapryor@gmail.com>
2024-03-14 16:38:41 -0400

    🧪 Add multiple checkpoints for createCourse

---
+++ test/Course.t.sol
@@ -0,0 +1,150 @@
+/*// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "../src/Course.sol";
+
+contract CourseTest is Test {
+    Course course;
+    address owner;
+    mapping(address => uint256) public startTimes;
+    mapping(address => uint256) public finishTimes;
+
+    function setUp() public {
+        course = new Course();
+        owner = msg.sender;
+    }
+
+    function test_AddCheckpoint() public {
+        course.addCheckpoint(123, 456);
+        course.addCheckpoint(654, 321);
+        course.build();
+        Course.Checkpoint[] memory checkpoints = course.getCheckpoints();
+        assertEq(checkpoints.length, 2);
+        assertEq(checkpoints[0].coord.lat, 123);
+        assertEq(checkpoints[0].coord.long, 456);
+        assertEq(checkpoints[0].sequence, 0);
+    }
+
+    function test_AddCheckpoints() public {
+        int256[] memory latitudes = new int256[](3);
+        latitudes[0] = 123456;
+        latitudes[1] = 234567;
+        latitudes[2] = 345678;
+        int256[] memory longitudes = new int256[](3);
+        longitudes[0] = 987654;
+        longitudes[1] = 876543;
+        longitudes[2] = 765432;
+        course.addCheckpoints(latitudes, longitudes);
+        course.build();
+        Course.Checkpoint[] memory checkpoints = course.getCheckpoints();
+        assertEq(checkpoints.length, 3);
+        assertEq(checkpoints[0].coord.lat, latitudes[0]);
+        assertEq(checkpoints[0].coord.long, longitudes[0]);
+        assertEq(checkpoints[0].sequence, 0);
+        assertEq(checkpoints[1].coord.lat, latitudes[1]);
+        assertEq(checkpoints[1].coord.long, longitudes[1]);
+        assertEq(checkpoints[1].sequence, 1);
+        assertEq(checkpoints[2].coord.lat, latitudes[2]);
+        assertEq(checkpoints[2].coord.long, longitudes[2]);
+        assertEq(checkpoints[2].sequence, 2);
+    }
+
+    function test_RemoveCheckpoint() public {
+        course.addCheckpoint(123, 456);
+        course.removeCheckpoint(0);
+        vm.expectRevert("At least two checkpoints must be added.");
+        course.build();
+    }
+
+    function test_Build() public {
+        course.addCheckpoint(123, 456);
+        course.addCheckpoint(101, 202);
+        course.build();
+        bool built = course.built();
+        assertEq(built, true);
+    }
+
+    function test_GetCheckpoints() public {
+        course.addCheckpoint(123, 456);
+        course.addCheckpoint(789, 101112);
+        course.build();
+        Course.Checkpoint[] memory checkpoints = course.getCheckpoints();
+        assertEq(checkpoints.length, 2);
+        assertEq(checkpoints[0].coord.lat, 123);
+        assertEq(checkpoints[0].coord.long, 456);
+        assertEq(checkpoints[0].sequence, 0);
+        assertEq(checkpoints[1].coord.lat, 789);
+        assertEq(checkpoints[1].coord.long, 101112);
+        assertEq(checkpoints[1].sequence, 1);
+    }
+
+    function test_OnlyOwnerModifier() public {
+        vm.prank(owner);
+        Course otherCourse = new Course();
+        course.addCheckpoint(654, 321);
+        try otherCourse.addCheckpoint(123, 456) {
+            assertTrue(false, "Function should have reverted.");
+        } catch Error(string memory reason) {
+            assertEq(reason, "Only the owner can perform this operation.");
+        } catch {
+            assertTrue(false, "Unexpected error.");
+        }
+        try otherCourse.removeCheckpoint(0) {
+            assertTrue(false, "Function should have reverted.");
+        } catch Error(string memory reason) {
+            assertEq(reason, "Only the owner can perform this operation.");
+        } catch {
+            assertTrue(false, "Unexpected error.");
+        }
+        try otherCourse.build() {
+            assertTrue(false, "Function should have reverted.");
+        } catch Error(string memory reason) {
+            assertEq(reason, "Only the owner can perform this operation.");
+        } catch {
+            assertTrue(false, "Unexpected error.");
+        }
+    }
+
+    function test_NotBuiltModifier() public {
+        course.addCheckpoint(123, 456);
+        course.addCheckpoint(654, 321);
+        course.build();
+        vm.expectRevert("Course has already been built.");
+        course.addCheckpoint(789, 101112);
+    }
+
+    function test_MarkCheckpointCompleted() public {
+        course.addCheckpoint(123, 456);
+        course.addCheckpoint(654, 321);
+        course.build();
+        vm.prank(owner);
+        course.startCourse();
+        course.markCheckpointCompleted(0, owner);
+    }
+
+    function test_StartCourse() public {
+        assertFalse(course.built());
+        course.addCheckpoint(123, 456);
+        course.addCheckpoint(654, 321);
+        course.build();
+        assertTrue(course.built());
+        course.startCourse();
+        assertEq(startTimes[msg.sender], block.timestamp - 1);
+    }
+
+    function test_FinishCourse() public {
+        course.addCheckpoint(123, 456);
+        course.addCheckpoint(654, 321);
+        course.build();
+        assertTrue(course.built());
+        vm.startPrank(owner);
+        course.startCourse();
+        startTimes[msg.sender] = block.timestamp;
+        course.markCheckpointCompleted(0, msg.sender);
+        course.markCheckpointCompleted(1, msg.sender);
+        finishTimes[msg.sender] = block.timestamp;
+        assertEq(finishTimes[msg.sender], block.timestamp);
+        vm.stopPrank();
+    }
+}*/
---
+++ test/CourseFactory.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "../src/CourseFactory.sol";
+import "../src/CheckpointFactory.sol";
+import "../src/interfaces/ICourseFactory.sol";
+import "../src/interfaces/ICheckpointFactory.sol";
+
+contract CourseFactoryTest is Test {
+    ICourseFactory courseFactory;
+    ICheckpointFactory checkpointFactory;
+
+    function setUp() public {
+        courseFactory = new CourseFactory();
+        checkpointFactory = new CheckpointFactory();
+    }
+
+    function testFuzz_createCourse(int64 lat1, int64 lng1, int64 lat2, int64 lng2) public {
+        vm.assume(lat1 != lat2 && lng1 != lng2);
+        vm.startPrank(address(1));
+        address checkpoint1 = checkpointFactory.createCheckpoint(lat1, lng1);
+        address checkpoint2 = checkpointFactory.createCheckpoint(lat2, lng2);
+
+        address[] memory checkpoints = new address[](2);
+        checkpoints[0] = checkpoint1;
+        checkpoints[1] = checkpoint2;
+
+        courseFactory.createCourse(checkpoints, 0);
+        vm.stopPrank();
+    }
+}
